3a40c793624d05ac5ab1eafd2dfebbb4830d4af6
Isaac <izhale@gmail.com>
2020-10-22 16:57:33 -0500

    compiling wang instances

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 import tweepy
 import re
 
+#Twitter API keys
 consumer_key = "aYvZwGA0cQybvozQmmJzy3a7U"
 consumer_secret = "zDZ3oDVzXQKx2ynIErL4xg8K6AbqrUSfmyZQscGDie42M9upCa"
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAGNIHAEAAAAAQf8mtmgLxvHsusDoa5PzpdkviME%3DQ7vf4dNyUpsbgaNnqhiILouK5WNvs6GSxwjUlMgQStAnTt3hTX"
@@ -13,26 +14,34 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
+#opening Harry Potter text file
 with open("Harry_Potter_and_the_Philosopher's_Stone.txt", "r", encoding = "utf8") as f:
     text = f.read()
 
+#stripping files of watermarks, Mrs. and Mr.
 text_watermark_stripped = re.sub(r'Page |[^.]*Harry Potter and the Philosophers Stone - J.K. Rowling', ' ', text)
 text_mrs_stripped = re.sub(r'Mrs.', 'Mrs', text_watermark_stripped)
 text_mr_stripped = re.sub(r'Mr\.', 'Mr', text_mrs_stripped)
 
+#replacing "wand" in its various forms with "wang"
 text_wang_replacement = re.sub(r'[Ww]and ','wang ', text_mr_stripped)
 text_plural_wang_replacement = re.sub(r'[Ww]ands ','wangs ', text_wang_replacement)
 text_period_wang_replacement = re.sub(r'[Ww]and\.', 'wang.', text_plural_wang_replacement)
 
-Hagrid_instance = re.compile(r'Hagrid')
-match = Hagrid_instance.findall(text)
+#finding all instances of the word "wang" along with its preceeding sentence
+wang_instance = re.compile(r'[^.?!]*[.?!][^.?!]*?wang[^.]*?[\.\?\!]')
+matches = wang_instance.findall(text_period_wang_replacement)
+
+#establish wang instance counter
+instance = 0
 
 i = 0
 while i <= 16:
     if i < 16:
         time.sleep(1)
-        print("Finding Hagrid in", 15 - i, "seconds.")
+        print("Finding wang instance in", 15 - i, "seconds.")
         i += 1
     if i == 16:
-        print(match)
+        print(matches[instance])
         i = 0
+        instance+=1 #increment wang instance counter
