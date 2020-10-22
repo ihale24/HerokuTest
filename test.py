6acffaa5225eee7b3dc2258f9737dc1a422d7c61
Isaac <izhale@gmail.com>
2020-10-22 15:13:20 -0500

    test

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 #This is a test.
+import time
 
-print("Hello Harry Potter. My name is Tom Riddle.")
+i = 0
+while i <= 16:
+    if i < 16:
+        time.sleep(1)
+        print("Tweeting in ", 15 - i, "seconds.")
+        i += 1
+    if i == 16:
+        print("TWEET")
+        i = 0
