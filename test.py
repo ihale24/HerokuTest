a17d30ecf63386a9cd75708dbd4723bde26800ae
Isaac <izhale@gmail.com>
2020-10-22 16:03:38 -0500

    Open .txt file

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 #This is a test.
 import time
 import tweepy
+import requests
+
+with open("GenericTextDoc.txt", "r", encoding = "utf-8") as f:
+    text = f.read()
 
 i = 0
 while i <= 16:
@@ -9,5 +13,5 @@ while i <= 16:
         print("Tweeting in ", 15 - i, "seconds.")
         i += 1
     if i == 16:
-        print("TWEET")
+        print(text)
         i = 0
