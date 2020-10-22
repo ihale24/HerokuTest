55ea56dee9a9d475b2499891d859d3c7b9bdfef4
Isaac <izhale@gmail.com>
2020-10-22 17:09:13 -0500

    Adding final tweet capabilities :)

--- test.py
+++ test.py
@@ -33,15 +33,18 @@ wang_instance = re.compile(r'[^.?!]*[.?!][^.?!]*?wang[^.]*?[\.\?\!]')
 matches = wang_instance.findall(text_period_wang_replacement)
 
 #establish wang instance counter
-instance = 0
+instance = 16
 
-i = 0
-while i <= 16:
-    if i < 16:
+#set up timer
+timer = 0
+while timer <= 86401: 
+    if timer < 86401: #while less than 86401, print countdown every second and increment timer
         time.sleep(1)
-        print("Finding wang instance in", 15 - i, "seconds.")
-        i += 1
-    if i == 16:
-        print(matches[instance])
-        i = 0
+        print("Tweeting next quote in", 86400 - timer, "seconds.")
+        timer += 1
+    if timer == 86401: #when timer hits 86401, Tweet th latest instance and print confirmation along with instance number
+        api.update_status(matches[instance])
+        print("Just tweeted this:" matches[instance])
+        print("Instance number:", instance)
+        timer = 0
         instance+=1 #increment wang instance counter
