2a04cad4c2e5383a463dd6e03aa6f050bb6a48a0
Isaac <izhale@gmail.com>
2020-10-22 16:49:41 -0500

    Stripping HP text with Regex

--- test.py
+++ test.py
@@ -16,6 +16,9 @@ api = tweepy.API(auth)
 with open("Harry_Potter_and_the_Philosopher's_Stone.txt", "r", encoding = "utf8") as f:
     text = f.read()
 
+text_watermark_stripped = re.sub(r'Page |[^.]*Harry Potter and the Philosophers Stone - J.K. Rowling', ' ', text)
+text_mrs_stripped = re.sub(r'Mrs.', 'Mrs', text_watermark_stripped)
+text_mr_stripped = re.sub(r'Mr\.', 'Mr', text_mrs_stripped)
 
 Hagrid_instance = re.compile(r'Hagrid')
 match = Hagrid_instance.findall(text)
@@ -24,7 +27,7 @@ i = 0
 while i <= 16:
     if i < 16:
         time.sleep(1)
-        print("Finding Hagrid ", 15 - i, "seconds.")
+        print("Finding Hagrid in", 15 - i, "seconds.")
         i += 1
     if i == 16:
         print(match)
